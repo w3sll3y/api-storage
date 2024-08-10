129c9f949a5b2af4499090be0de140298a768eb8
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-08-10 11:25:21 -0300

    chore(user): generate resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
