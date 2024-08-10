e69918bfd73e8f2d761df2023fe0c92e58124d0e
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-08-10 18:33:35 -0300

    chore(cards): create module, controller and service cards

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { UserModule } from './user/user.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PurchasesModule } from './purchases/purchases.module';
+import { CardsModule } from './cards/cards.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, PurchasesModule],
+  imports: [PrismaModule, UserModule, AuthModule, PurchasesModule, CardsModule],
   controllers: [AppController],
   providers: [
     AppService,
---
+++ src/cards/cards.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { CardsService } from './cards.service';
+
+@Controller('cards')
+export class CardsController {
+  constructor(private readonly cardsService: CardsService) { }
+
+  @Get()
+  findAll() {
+    return this.cardsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.cardsService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.cardsService.remove(+id);
+  }
+}
